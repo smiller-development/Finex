546db1756624f7679c6423302f031599109be648
Sam Miller <23262411+s-mil@users.noreply.github.com>
2025-03-03 09:33:41 -0600

    init

---
+++ devenv.nix
@@ -0,0 +1,41 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  languages.go.enable = true;
+  languages.typescript.enable = true;
+  languages.javascript = {
+    enable = true;
+    npm.enable = true;
+  };
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  services.postgres = {
+    enable = true;
+
+    initialDatabases = [{ name = "book";}];
+
+
+  };
+
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
